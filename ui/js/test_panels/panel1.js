eb1be91b7f004137295b4f0ebb1683c579573efe
art <arturvachev@gmail.com>
2024-02-14 23:04:12 +0300

    Revert "1 task"
    This reverts commit 6c7a8d50badc1e073f5982e514cea9382ab6833f.

--- ui/js/test_panels/panel1.js
+++ ui/js/test_panels/panel1.js
@@ -1,17 +1,3 @@
-function buttonTest() {
-    return new Ext.Button({
-        text: 'Button №2',
-        id: 'button2',
-        handler: function () {
-            Ext.getCmp('button2').hide();
-
-            setTimeout(function () {
-                Ext.getCmp('button1').show();
-            }, 1500)
-        }
-    });
-}
-
 panel1 = new Ext.Panel({
     title: 'Задание 1',
     listeners: {
@@ -45,26 +31,7 @@ panel1 = new Ext.Panel({
             xtype: 'panel',
             flex: 1,
             padding: 10,
-            id: 'main',
-            items: [
-                {
-                    xtype: 'button',
-                    text: 'Button №1',
-                    id: 'button1',
-                    handler: function () {
-                        let panel = Ext.getCmp('main');
-                        let button = buttonTest();
-
-                        panel.add(button);
-                        panel.doLayout();
-
-                        setTimeout(function () {
-                            Ext.getCmp('button1').hide();
-                        },2000)
-                    }
-                }
-            ]
+            html: 'Тут решение'
         }
     ]
 });
-
